da6976df6d64e7135961efc20154356a49c004dd
HonggugangChen <chenhon@chalmers.se>
2023-09-16 22:01:29 +0200

    update lab3 csv2latex script

--- lab_3/csv2latex.py
+++ lab_3/csv2latex.py
@@ -3,6 +3,7 @@ import pandas as pd
 csv_file = 'extract.csv'
 
 df = pd.read_csv(csv_file)
+df = df.sort_values(by=[' data_size(MB)','thread_num'])
 
 latex_table = df.to_latex(index=False,escape=False)
 print(latex_table)
